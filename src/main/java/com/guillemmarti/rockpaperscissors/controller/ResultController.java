c0c198c1c768bcf8922030e2b55cf258d4919f80
Guillem Martí <w_floppy@hotmail.com>
2023-06-18 13:19:04 +0200

    fix typing error on controller

--- src/main/java/com/guillemmarti/rockpaperscissors/controller/ResultController.java
+++ src/main/java/com/guillemmarti/rockpaperscissors/controller/ResultController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
-public class ResultController {cd ..
+public class ResultController {
 
     private final DecisionService decisionService;
 
